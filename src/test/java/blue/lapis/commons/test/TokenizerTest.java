a3dc54b7b27a9b0235d681bbf53965ff71ed4785
Minecrell <dev@minecrell.net>
2014-09-25 19:08:07 +0200

    Clean up the test class.

--- src/test/java/blue/lapis/commons/test/TokenizerTest.java
+++ src/test/java/blue/lapis/commons/test/TokenizerTest.java
@@ -25,6 +25,7 @@ package blue.lapis.commons.test;
 import blue.lapis.common.LapisCommonsPlugin;
 import blue.lapis.common.command.impl.PlayerTokenParser;
 import blue.lapis.common.command.impl.StandardTokenizer;
+import com.google.common.collect.Lists;
 import org.apache.logging.log4j.LogManager;
 import org.junit.Assert;
 import org.junit.Before;
@@ -33,58 +34,50 @@ import org.spongepowered.api.Game;
 import org.spongepowered.api.entity.Player;
 import org.spongepowered.api.event.state.PreInitializationEvent;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-/**
- *
- */
 public class TokenizerTest {
-    LapisCommonsPlugin plugin = new LapisCommonsPlugin();
-    public StandardTokenizer tokenizer = new StandardTokenizer();
-    public PlayerTokenParser playerParser = new PlayerTokenParser();
-    public Game gameProxy;
-
-    public String identifiers = "one two three four 81 {} six [ ]";
-    public String quotes = "one \"two three\" four 81 {} [ ]";
+    private StandardTokenizer tokenizer = new StandardTokenizer();
 
     @Before
     public void initTests() {
-        List<Player> playerList = new ArrayList<Player>();
-        for (String name : new String[]{"Foo", "Bar", "Baz", "Lorem", "Ipsum", "Marco", "Polo", "Foot"}) {
-            Player p = mock(Player.class);
-            when(p.getName()).thenReturn(name);
-            playerList.add(p);
-        }
-        gameProxy = mock(Game.class);
-        when(gameProxy.getOnlinePlayers()).thenReturn(playerList);
-
+        Game gameProxy = mock(Game.class);
         PreInitializationEvent init = mock(PreInitializationEvent.class);
         when(init.getGame()).thenReturn(gameProxy);
         when(init.getPluginLog()).thenReturn(LogManager.getLogger(LapisCommonsPlugin.class));
-        plugin.initialize(init);
+        new LapisCommonsPlugin().initialize(init);
     }
 
     @Test
     public void identifiers() {
-        String[] tokens = tokenizer.getTokens(identifiers);
+        String[] tokens = tokenizer.getTokens("one two three four 81 {} six [ ]");
         Assert.assertArrayEquals(new String[]{"one", "two", "three", "four", "81.0", "six"}, tokens);
     }
 
     @Test
     public void quotes() {
-        String[] tokens = tokenizer.getTokens(quotes);
+        String[] tokens = tokenizer.getTokens("one \"two three\" four 81 {} [ ]");
         Assert.assertArrayEquals(new String[]{"one", "two three", "four", "81.0"}, tokens);
     }
 
     @Test
     public void resolvePlayer() {
-        String resolution = playerParser.parse(null, "Lo").getName();
+        List<Player> playerList = Lists.newArrayList();
+        for (String name : new String[]{"Foo", "Bar", "Baz", "Lorem", "Ipsum", "Marco", "Polo", "Foot"}) {
+            Player p = mock(Player.class);
+            when(p.getName()).thenReturn(name);
+            playerList.add(p);
+        }
+
+        when(LapisCommonsPlugin.getGame().getOnlinePlayers()).thenReturn(playerList);
+
+        PlayerTokenParser parser = new PlayerTokenParser();
+        String resolution = parser.parse(null, "Lo").getName();
         Assert.assertEquals("Lorem", resolution);
-        resolution = playerParser.parse(null, "Foo").getName();
+        resolution = parser.parse(null, "Foo").getName();
         Assert.assertEquals("Foo", resolution);
     }
 
