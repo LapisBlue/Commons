41247ceb3e5099bb60a4c09849dd3cc8001be187
gratimax <max@ovsankin.com>
2014-09-27 19:26:35 -0700

    updated license headers

--- src/main/java/blue/lapis/common/command/impl/BooleanTokenParser.java
+++ src/main/java/blue/lapis/common/command/impl/BooleanTokenParser.java
@@ -1,6 +1,6 @@
 /*
  * LapisCommons
- * Copyright (c) 2014, LapisDev <https://github.com/LapisDev>
+ * Copyright (c) 2014, Lapis <https://github.com/LapisBlue>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
