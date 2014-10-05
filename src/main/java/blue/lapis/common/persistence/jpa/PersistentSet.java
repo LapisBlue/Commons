7cd26f431e46b7067816fd0b88c21318709cf672
Thomas Richner <thomasrichner+github@gmail.com>
2014-10-05 05:42:53 +0200

    added persistent set

---
+++ src/main/java/blue/lapis/common/persistence/jpa/PersistentSet.java
@@ -0,0 +1,11 @@
+package blue.lapis.common.persistence.jpa;
+
+import java.util.Set;
+
+/**
+ * Created on 05.10.2014.
+ * @author Thomas
+ */
+public interface PersistentSet<T> extends PersistentCollection,Set<T> {
+
+}
